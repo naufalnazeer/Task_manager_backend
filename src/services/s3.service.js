2fda4c8f2cb400b5aea5967fea4d52e53eff189f
Naufal Nazeer <naufal@Naufals-MacBook-Pro.local>
2026-05-14 15:54:35 +0530

    update bucket logic

--- src/services/s3.service.js
+++ src/services/s3.service.js
@@ -1,18 +1,24 @@
-const { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { S3Client, PutObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
 const crypto = require('crypto');
 const path = require('path');
 
-const s3Client = new S3Client({
-  endpoint: process.env.SUPABASE_S3_ENDPOINT,
-  region: process.env.SUPABASE_S3_REGION,
-  credentials: {
-    accessKeyId: process.env.SUPABASE_S3_ACCESS_KEY,
-    secretAccessKey: process.env.SUPABASE_S3_SECRET_KEY,
-  },
-  forcePathStyle: true,
-});
+// Lazy-init the S3 client so env vars are available after dotenv.config()
+let s3Client;
 
-const BUCKET = process.env.SUPABASE_S3_BUCKET;
+const getS3Client = () => {
+  if (!s3Client) {
+    s3Client = new S3Client({
+      endpoint: process.env.SUPABASE_S3_ENDPOINT,
+      region: process.env.SUPABASE_S3_REGION || 'ap-southeast-1',
+      credentials: {
+        accessKeyId: process.env.SUPABASE_S3_ACCESS_KEY,
+        secretAccessKey: process.env.SUPABASE_S3_SECRET_KEY,
+      },
+      forcePathStyle: true,
+    });
+  }
+  return s3Client;
+};
 
 /**
  * Upload a file to Supabase S3
@@ -23,21 +29,26 @@ const BUCKET = process.env.SUPABASE_S3_BUCKET;
  * @returns {object} - { key, url }
  */
 const uploadFile = async (fileBuffer, originalName, mimeType, folder) => {
+  const bucket = process.env.SUPABASE_S3_BUCKET;
   const uniqueId = crypto.randomUUID();
   const ext = path.extname(originalName);
   const key = `${folder}/${uniqueId}${ext}`;
 
   const command = new PutObjectCommand({
-    Bucket: BUCKET,
+    Bucket: bucket,
     Key: key,
     Body: fileBuffer,
     ContentType: mimeType,
   });
-
-  await s3Client.send(command);
+try {
+  await getS3Client().send(command);
+    
+} catch (error) {
+    console.log(error)
+}
 
   // Construct the public URL
-  const url = `${process.env.SUPABASE_S3_ENDPOINT.replace('/s3', '')}/${BUCKET}/${key}`;
+  const url = `${process.env.SUPABASE_S3_ENDPOINT.replace('/s3', '')}/${bucket}/${key}`;
 
   return { key, url };
 };
@@ -47,12 +58,14 @@ const uploadFile = async (fileBuffer, originalName, mimeType, folder) => {
  * @param {string} key - The S3 object key
  */
 const deleteFile = async (key) => {
+  const bucket = process.env.SUPABASE_S3_BUCKET;
+
   const command = new DeleteObjectCommand({
-    Bucket: BUCKET,
+    Bucket: bucket,
     Key: key,
   });
 
-  await s3Client.send(command);
+  await getS3Client().send(command);
 };
 
-module.exports = { uploadFile, deleteFile, s3Client };
+module.exports = { uploadFile, deleteFile };
